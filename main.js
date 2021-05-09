dc62876f0ba98b36945bc0637ee66d8bded30caf
davincikab <davidnganganjeri079@gmail.com>
2021-05-09 12:43:01 -0700

    layer checkbox

--- main.js
+++ main.js
@@ -6,31 +6,41 @@ var map = new mapboxgl.Map({
     zoom: 3
 });
  
-var network = [
+var network250 = [
     {id:'', radius:250, coordinate:[2.4313527, 33.6855357]},
+];
+
+var network80 = [
     {id:'', radius:80, coordinate:[-42.2212446, 41.40573587]},
     // {id:'', radius:250, coordinate:[33.6855357,2.4313527]}
 ];
 
-
 // create circles
 let features = [];
-network.forEach(entry => {
+network250.forEach(entry => {
     let circle = turf.circle(entry.coordinate, entry.radius, {properties:entry});
     features.push(circle);
 });
 
-var networkGrid = turf.featureCollection(features);
+var networkGrid250 = turf.featureCollection(features);
+
+let features80 = [];
+network80.forEach(entry => {
+    let circle = turf.circle(entry.coordinate, entry.radius, {properties:entry});
+    features80.push(circle);
+});
+
+var networkGrid80 = turf.featureCollection(features80);
 
 map.on('load', function () {
-    map.addSource("network", {
+    map.addSource("network-grid-250", {
         type: 'geojson',
-        'data':networkGrid
+        'data':networkGrid250
     });
 
     map.addLayer({
-        id:"network-grid",
-        source:"network",
+        id:"network-grid-250",
+        source:"network-grid-250",
         type:"fill",
         paint:{
             'fill-color':'#26fbca',
@@ -38,4 +48,85 @@ map.on('load', function () {
         }
     });
 
+    map.addSource("network-grid-80", {
+        type: 'geojson',
+        'data':networkGrid80
+    });
+
+    map.addLayer({
+        id:"network-grid-80",
+        source:"network-grid-80",
+        type:"fill",
+        paint:{
+            'fill-color':'#26fbca',
+            'fill-opacity':0.55
+        }
+    });
+
+    // add
+
+});
+
+// 
+var layers = {
+    '250 Km':'network-grid-250',
+    '80 Km':'network-grid-80'
+};
+
+class LayerControl {
+    constructor(layers) {
+        this.layers = layers;
+    }
+
+    toggleLayer(layerId, checked) {
+        console.log("toggling layer");
+        console.log(layerId);
+
+        var visibility = checked ? 'visible' : 'none';
+        this._map.getLayer(layerId) ? this._map.setLayoutProperty(layerId, 'visibility', visibility) : false;
+    }
+
+    onAdd(map) {
+        this._map = map;
+
+        this._container = document.createElement("div");
+        this._container.className = 'mapboxgl-ctrl layer-control';
+
+        Object.keys(this.layers).forEach(key => {
+            let layer = this.layers[key];
+
+            let layerDiv = document.createElement("div");
+
+            let layerCheckbox = document.createElement("input");
+            layerCheckbox.type = "checkbox";
+            layerCheckbox.className = "layer-element";
+            layerCheckbox.value = layer;
+            layerCheckbox.setAttribute("checked", true);
+            // layerCheckbox.id = layer;
+
+            layerDiv.append(layerCheckbox);
+            layerDiv.innerHTML += "<label>"+ key +"</label>"
+
+            this._container.append(layerDiv);
+        });
+
+        return this._container;
+    }
+
+    onRemove() {
+        this._container.parentNode.removeChild(this._container);
+        this._map = undefined;
+    }
+}
+
+var layerControl = new LayerControl(layers);
+map.addControl(layerControl, 'top-right');
+
+
+var layerCheckboxes = document.querySelectorAll(".layer-element");
+layerCheckboxes.forEach(layerCheckbox => {
+    layerCheckbox.onchange = function(e) {
+        let { value, checked } = e.target;
+        layerControl.toggleLayer(value, checked);
+    }
 });
